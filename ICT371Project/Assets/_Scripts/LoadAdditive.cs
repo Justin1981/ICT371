162452f364fbd74ded5f2acd463fab5ae6033dd7
Justin1981 <justin.pettit@hotmail.com.au>
2016-04-27 10:32:48 +0800

    A1 dump - Untested

---
+++ ICT371Project/Assets/_Scripts/LoadAdditive.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Script to load and add a scene to the existing scene
+/// </summary>
+public class LoadAdditive : MonoBehaviour
+{
+
+    public void LoadAddOnClick(int level)
+    {
+        //Application.LoadLevelAdditive(level);
+        SceneManager.LoadScene(level, LoadSceneMode.Additive);
+    }
+}
---
+++ ICT371Project/Assets/_Scripts/StartPanelScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+/// <summary>
+/// Script to disable the canvas shown when the scene first loads.
+/// NOTE: Canvas used here to enable/disable canvas which is 
+/// not as good as acting on the GameObject. THIS METHOD SHOULD NOT BE USED
+/// </summary>
+public class StartPanelScript : MonoBehaviour {
+    
+    public Canvas startCanvas;
+    public Button okBtn;
+
+    // Use this for initialization
+    void Start()
+    {
+        startCanvas = startCanvas.GetComponent<Canvas>();
+        okBtn = okBtn.GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    public void StartPanelOff()
+    {
+        startCanvas.enabled = false;
+    }
+
+}
