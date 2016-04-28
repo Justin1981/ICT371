f4549651b5a9d9942c4d07caceb7e856edbe256b
Justin1981 <justin.pettit@hotmail.com.au>
2016-04-28 10:24:29 +0800

    Init Testing 4 New Unity Project

---
+++ ICT371Project/Assets/_Scripts/ExitScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Script to exit the application
+/// </summary>
+public class ExitScript : MonoBehaviour
+{
+
+    // Use to exit application
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ ICT371Project/Assets/_Scripts/InfoPanelScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+/// <summary>
+/// Script to enable/disable the info panel when the user
+/// clicks on the model on the screen.
+/// NOTE: GameObject used here to enable/disable canvas which is 
+/// better then just acting on the canvas. THIS METHOD SHOULD BE USED
+/// </summary>
+public class InfoPanelScript : MonoBehaviour {
+
+    //public Canvas infoCanvas;
+    public GameObject infoCanvas;
+    public Button okBtn;
+
+    private bool startPanelDisabled;
+
+	// Use this for initialization
+	void Start () 
+    {
+        //infoCanvas = infoCanvas.GetComponent<Canvas>();
+        //infoCanvas.enabled = false;
+
+        okBtn = okBtn.GetComponent<Button>();
+
+        startPanelDisabled = false;
+	}
+	
+	// Turn info panel off
+    public void InfoPanelOff()
+    {
+        //infoCanvas.enabled = false;
+        infoCanvas.SetActive(false);
+    }
+
+    public void StartPanelDisabled()
+    {
+        startPanelDisabled = true;
+    }
+
+    void OnMouseDown()
+    {
+        //Debug.Log("OnMouseDown Called");
+        //if (!infoCanvas.enabled)
+        //{
+        //    infoCanvas.enabled = true;
+        //}
+        if (!infoCanvas.activeSelf && startPanelDisabled)
+        {
+            infoCanvas.SetActive(true);
+        }
+    }
+}
